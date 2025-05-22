a41f905720f2a65dd90760c70d98fda20ba911ee
Laagren <simonagren@hotmail.se>
2025-05-22 13:15:11 +0200

    added server error view

--- ErrorHandling.Web/Controllers/ErrorController.cs
+++ ErrorHandling.Web/Controllers/ErrorController.cs
@@ -5,10 +5,16 @@ namespace ErrorHandling.Web.Controllers
     public class ErrorController : Controller
     {
         [HttpGet("error/exception")]
-        public IActionResult ServerError()
+        public IActionResult ServerException()
         {
             return View();
         }
 
+
+        [HttpGet("error/http/{code}")]
+        public IActionResult HttpError(int code)
+        {
+            return View(code);
+        }
     }
 }
