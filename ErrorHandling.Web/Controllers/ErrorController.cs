07eeac29d153c04117796a1b11eee9fcb8225a5c
Ghassan01 <Ghassan01@GHASSAN>
2025-05-22 11:30:13 +0200

    Skapa Home controller och Error controller

---
+++ ErrorHandling.Web/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandling.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet("error/exception")]
+        public IActionResult ServerError()
+        {
+            return View();
+        }
+
+    }
+}
